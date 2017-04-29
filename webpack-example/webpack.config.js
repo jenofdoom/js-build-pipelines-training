c594e26dd9d927111dc72ee199878c0f3aaeeb11
jenofdoom <j_zajac@email.com>
2017-04-29 12:53:51 +1200

    Webpack config uses webpack-merge

--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -3,20 +3,19 @@ const autoprefixer = require('autoprefixer');
 const flexfixes = require('postcss-flexbugs-fixes');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
 
-const config = {
+const env = process.env.npm_lifecycle_event === 'build' ? 'prod' : 'dev';
+let config = {};
+
+// config that is shared between all types of build (dev and prod)
+const common = {
   entry: ['babel-polyfill', './src/index.jsx'],
 
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-
-  devServer: {
-    historyApiFallback: true // enables reloads of routed pages
-  },
-
-  devtool: process.env.npm_lifecycle_event === 'build' ? 'cheap-module-source-map' : 'cheap-module-eval-source-map',
 
   module: {
     rules: [
@@ -32,31 +31,6 @@ const config = {
         loader: 'babel-loader' // config in .babelrc
       },
       {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: [
-                  autoprefixer({browsers: ['last 2 versions']}),
-                  flexfixes()
-                ]
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                includePaths: [path.resolve(__dirname, 'src')]
-              }
-            }
-          ]
-        })
-      },
-      {
         test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         exclude: /node_modules/,
         loader: 'url-loader?limit=10000' // will insert a data URI if filesize < 10kb otherwise uses file-loader
@@ -68,8 +42,7 @@ const config = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       hash: true
-    }),
-    new ExtractTextPlugin('bundle.css')
+    })
   ],
 
   resolve: {
@@ -77,5 +50,91 @@ const config = {
     modules: [path.resolve(__dirname, 'src'), 'node_modules']
   }
 };
+
+// environment specific config
+switch (env) {
+  case 'dev':
+    config = merge(common, {
+      devtool: 'cheap-module-eval-source-map',
+
+      devServer: {
+        historyApiFallback: true // enables reloads of routed pages
+      },
+
+      // because we need to use ExtractTextPlugin for prod, we have to specify the 'dev' scss test here
+      // rather than in common, or else they get merged weirdly
+      module: {
+        rules: [
+          {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            use: [
+              'style-loader',
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  plugins: [
+                    autoprefixer({browsers: ['last 2 versions']}),
+                    flexfixes()
+                  ]
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  includePaths: [path.resolve(__dirname, 'src')]
+                }
+              }
+            ]
+          }
+        ]
+      }
+    });
+    break;
+  case 'prod':
+    // most of the prod specific config is provided directly by webpack as we supplied the -p flag
+    // but we want to only use ExtractTextPlugin for prod, not dev
+    config = merge(common, {
+      devtool: 'cheap-module-source-map',
+
+      module: {
+        rules: [
+          {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: [
+                'css-loader',
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    plugins: [
+                      autoprefixer({browsers: ['last 2 versions']}),
+                      flexfixes()
+                    ]
+                  }
+                },
+                {
+                  loader: 'sass-loader',
+                  options: {
+                    includePaths: [path.resolve(__dirname, 'src')]
+                  }
+                }
+              ]
+            })
+          }
+        ]
+      },
+
+      plugins: [
+        new ExtractTextPlugin('bundle.css')
+      ]
+    });
+    break;
+  default:
+    config = common;
+  }
 
 module.exports = config;
